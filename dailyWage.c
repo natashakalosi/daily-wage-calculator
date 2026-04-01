a61e87dea86c45909479c03aa376dcffac610943
Natasha Kalosi <kalosinatasha@gmail.com>
2026-04-01 23:45:42 +0200

    Add files via upload

---
+++ dailyWage.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int hours, total;
+    int hourly_rate = 10;
+    int overtime_rate = 15; 
+
+    printf("Enter total hours worked today: ");
+    if (scanf("%d", &hours) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    if (hours > 8) {
+        // 8 ώρες με κανονικό μισθό + οι υπόλοιπες με υπερωρία
+        total = (8 * hourly_rate) + ((hours - 8) * overtime_rate);
+        printf("You worked overtime! Your wage today is: %d $\n", total);
+    } 
+    else if (hours > 0) {
+        // Πληρωμή μόνο για τις ώρες που δούλεψε (έως 8)
+        total = hours * hourly_rate;
+        printf("Your wage today is: %d $\n", total);
+    } 
+    else {
+        printf("No hours worked today.\n");
+    }
+
+    return 0;
+}
